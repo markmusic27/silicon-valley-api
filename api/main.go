d0ecad459165741de341377afb2ab75ba66942df
Mark Music <markmusic999@gmail.com>
2021-01-22 10:50:08 -0600

    ft[structure] => 🪐 Build JSON Article structure

--- api/main.go
+++ api/main.go
@@ -6,12 +6,20 @@ import (
 	"net/http"
 )
 
-func homepage(w http.ResponseWriter, r *http.Request){
+type Article struct {
+	Quote string `json:"Quote"`
+	SaidBy string `json:"SaidBy"`
+	Context string `json:"Context"`
+	Episode string `json:"Episode"`
+	Season string `json:"Season"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Homepage Endpoint hit")
 }
 
 func handleRequest(){
-	http.HandleFunc("/", homepage)
+	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
