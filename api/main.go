985697fdb7ea2ec6516669481d9bae36d93628ca
Mark Music <markmusic999@gmail.com>
2021-01-22 11:18:13 -0600

    ft[homePage] =>> ⚡️ Build homePage endpoint

--- api/main.go
+++ api/main.go
@@ -7,7 +7,7 @@ import (
 	"encoding/json"
 )
 
-type Article struct {
+type Quote struct {
 	Quote string `json:"Quote"`
 	SaidBy string `json:"SaidBy"`
 	Context string `json:"Context"`
@@ -15,24 +15,24 @@ type Article struct {
 	Season int `json:"Season"`
 }
 
-type Articles []Article
+type Quotes []Quote
 
-func allArticles(w http.ResponseWriter, r *http.Request){
-	articles := Articles{
-	Article{Quote:"Test", SaidBy: "Test", Context: "Test", Episode: 1, Season: 6},
+func allQuotes(w http.ResponseWriter, r *http.Request){
+	quotes := Quotes{
+	Quote{Quote:"Test", SaidBy: "Test", Context: "Test", Episode: 1, Season: 6},
 	}
 
 	fmt.Println(w, "Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+	json.NewEncoder(w).Encode(quotes)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Homepage Endpoint hit")
+	fmt.Fprintf(w, "Welcome to the Silicon Valley API! Here is a list of all of the endpoints:\n\n- ⚡️ All Quotes: /quotes\n- 🧩 Random Quote: /randquote\n\nCheckout the source code at https://github.com/markmusic2727/silicon-valley_api. All contributions are welcome!")
 }
 
 func handleRequest(){
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
+	http.HandleFunc("/quotes", allQuotes)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
